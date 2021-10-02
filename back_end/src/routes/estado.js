effab3d0708e49a70f32031e80e0db100bea0f5a
wenderu00 <mwmcjr@gmail,com>
2021-10-02 13:20:46 -0300

    adição do metodo patch ao router estado

--- back_end/src/routes/estado.js
+++ back_end/src/routes/estado.js
@@ -51,4 +51,30 @@ router.post('/',gerente,(req,res,next)=>{
     })
 })
 
+router.patch('/',gerente,(req,res,next)=>{
+    pool.getConnection((error,conn)=>{
+        if(error){return res.status(500).send({error:error})}
+        conn.query('SELECT * FROM estado WHERE id_estado = ?',[req.body.id_estado],(error,results,fields)=>{
+            if(error){return res.status(500).send({error:error})}
+            if(results.length == 0){return res.status(404).send({mensagem: "estado não encontrado"})}
+            conn.query('SELECT * FROM estado WHERE nome = ?',[req.body.nome],(error,result,field)=>{
+                if(error){return res.status(500).send({error:error})}
+                if(result.length>0){return res.status(409).send({mensagem:"estado já cadastrado"})}
+                conn.query('UPDATE estado SET nome = ? WHERE id_estado = ?',[req.body.nome,req.body.id_estado],(error,resul,fiel)=>{
+                    if(error){return res.status(500).send({error:error})}
+                    const response = {
+                        mensagem:"estado alterado com sucesso",
+                        estadoGerado: {
+                            id_estado: req.body.id_estado,
+                            nome: req.body.nome
+                        }
+                    
+                    }
+                    return res.status(201).send(response)
+                })
+            })
+        })
+    })
+})
+
 module.exports = router
