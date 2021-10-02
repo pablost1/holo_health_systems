8bfa363734d903bd547c6ca037dfd01eccfd82a4
wenderu00 <mwmcjr@gmail,com>
2021-10-02 14:58:05 -0300

    rotas de cidade

---
+++ back_end/src/routes/cidade.js
@@ -0,0 +1,107 @@
+const express = require('express');
+const router = express.Router();
+const pool = require("../mysql").pool;
+
+const paci_op = require('../middleware/login_paciente_op') 
+const gerente = require('../middleware/login_gerente')
+
+router.get('/',paci_op,(req,res,next)=>{
+    pool.getConnection((error,conn)=>{
+        if(error){return res.status(500).send({error:error})}
+        conn.query('SELECT * FROM Cidade',(error,results,field)=>{
+            if(error){return res.status(500).send({error:error})}
+            const response = {
+                Cidades: results.map(cidade =>{
+                    return {
+                        id_cidade: cidade.id_cidade,
+                        id_estado: cidade.id_estado,
+                        nome: cidade.nome
+
+                    }
+                })
+            }
+            return res.status(200).send(response)
+        })
+    })
+})
+
+router.post('/',gerente,(req,res,next)=>{
+    pool.getConnection((error,conn)=>{
+        if(error){return res.status(500).send({error:error})}
+        conn.query(
+            'SELECT * FROM estado WHERE id_estado = ?',
+            [req.body.id_estado],
+            (error,results,fields)=>{
+                if(error){return res.status(500).send({error:error})}
+                if(results.length == 0){return res.status(404).send({mensagem: "estado não encontrado"})}
+                conn.query(
+                    'SELECT * FROM Cidade WHERE nome = ?',
+                    [req.body.nome],
+                    (error,result, field)=>{
+                        if(error){return res.status(500).send({error:error})}
+                        if(result.length!=0){return res.status(409).send({mensagem:"cidade já cadastrada"})}
+                        conn.query(
+                            'INSERT INTO Cidade (nome,id_estado) VALUES (?,?)',
+                            [req.body.nome,req.body.id_estado],
+                            (error,resul,fiel)=>{
+                                if(error){return res.status(500).send({error:error})}
+                                const response = {
+                                    mensagem:"Cidade criada com sucesso",
+                                    cidadeCriada:{
+                                        id_cidade: resul.insertId,
+                                        nome: req.body.nome,
+                                        id_estado: req.body.estado
+                                    }
+                                }
+                                return res.status(201).send(response)
+                            }
+                        )
+                    }
+                )
+            }
+            )
+    })
+})
+
+router.patch('/',gerente,(req,res,next)=>{
+    pool.getConnection((error,conn)=>{
+        if(error){return res.status(500).send({error:error})}
+        conn.query('SELECT * FROM Cidade WHERE id_cidade = ?', [req.body.id_cidade],(error,results,fields)=>{
+                
+            if(error){return res.status(500).send({error:error})}
+            if(results.length==0){return res.status(404).send({mensagem:"cidade não encontrada"})}
+            conn.query('SELECT * FROM Cidade WHERE nome = ?',[req.body.nome],(error,result,field)=>{
+                if(error){return res.status(500).send({error:error})}
+                if(result.length>0){return res.status(409).send({mensagem:"cidade já cadastrada"})}
+                conn.query('UPDATE Cidade SET nome = ? WHERE id_cidade = ?',[req.body.nome,req.body.id_cidade],(error,resul,fiel)=>{
+                    conn.release()
+                    if(error){return res.status(500).send({error:error})}
+                    const response = {
+                        mensagem:"cidade alterada com sucesso",
+                        estadoGerado: {
+                            id_cidade: req.body.id_cidade,
+                            nome: req.body.nome
+                        }
+                    
+                    }
+                    return res.status(202).send(response)
+                })
+            })
+        })
+    })
+})
+
+router.delete('/',gerente,(req,res,next)=>{
+    pool.getConnection((error,conn)=>{
+        if(error){return res.status(500).send({error:error})}
+        conn.query(
+            'DELETE FROM Cidade WHERE id_cidade = ?',
+            [req.body.id_cidade],
+            (error,result,field)=>{
+                if(error){return res.status(500).send({error:error})}
+                return res.status(202).send({mensagem:"removido com sucesso"})
+            }
+        )
+    })
+})
+module.exports = router
