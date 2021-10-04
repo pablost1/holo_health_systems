419670405c5efe6732a940bc41e05ef2644901fe
wenderu00 <mwmcjr@gmail,com>
2021-10-04 11:20:48 -0300

    rota de especialidade_consultorio

---
+++ back_end/src/routes/especonsul.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const router = express.Router();
+const pool = require("../mysql").pool;
+
+const paci_op = require('../middleware/login_paciente_op') 
+const gerente = require('../middleware/login_gerente')
+
+router.get('/',paci_op,(req,res,next)=>{
+    pool.getConnection((error,conn)=>{
+        if(error){return res.status(500).send({error:error})}
+        conn.query('SELECT * FROM Especialidade_Consultorio',(error,results,fields)=>{
+            conn.release()
+            if(error){return res.status(500).send({error:error})}
+            const response = {
+                especonsuls: results.map(especonsul => {
+                    return {
+                        id_Especialidade_Consultorio: especonsul.id_Especialidade_Consultorio,
+                        id_consultorio: especonsul.id_consultorio,
+                        id_especialidade: especonsul.id_especialidade
+                    }
+                })
+            }
+            return res.status(200).send(response)
+        })
+    })
+})
+
+router.post('/',gerente,(req,res,next)=>{
+    pool.getConnection((error,conn)=>{
+        if(error){return res.status(500).send({error:error})}
+        conn.query(
+            'SELECT * FROM Consultorio WHERE id_consultorio = ?',
+            [req.body.id_consultorio],
+            (error,results,fields)=>{
+                if(error){return res.status(500).send({error:error})}
+                if(results.length == 0 ){return res.status(404).send({mensagem: "consultorio não encontrado"})}
+                conn.query(
+                    'SELECT * FROM Especialidade WHERE id_especialidade = ?',
+                    [req.body.id_consultorio],
+                    (error,results,fields)=>{
+                        if(error){return res.status(500).send({error:error})}
+                        if(results.length == 0 ){return res.status(404).send({mensagem: "Especialidade não encontrada"})}
+                        conn.query(
+                            'SELECT * FROM Especialidade_Consultorio WHERE id_consultorio = ? AND id_especialidade = ?',
+                            [req.body.id_consultorio,req.body.id_especialidade],
+                            (error,results,fields)=>{
+                                if(error){return res.status(500).send({error:error})}
+                                if(results.length!= 0){return res.status(409).send({mensagem:"essa ligação especialidade/consultorio já foi cadastrada"})}
+                                conn.query(
+                                    'INSERT INTO Especialidade_Consultorio (id_consultorio,id_especialidade) VALUES (?,?)',
+                                    [req.body.id_consultorio,req.body.id_especialidade],
+                                    (error,results,fields)=>{
+                                        conn.release()
+                                        if(error){return res.status(500).send({error:error})}
+                                        const response = {
+                                            mensagem: "Especialidade_Consultorio criado com sucesso",
+                                            especonsulCriado: {
+                                                id_Especialidade_Consultorio: results.insertId,
+                                                id_consultorio: req.body.id_consultorio,
+                                                id_especialidade: req.body.id_especialidade
+                                            }
+                                        }
+                                        return res.status(201).send(response)
+                                    }
+                                    )
+                            }
+                        )   
+                    }
+                )
+                
+            }
+        )
+    })
+})
+
+
+module.exports = router
