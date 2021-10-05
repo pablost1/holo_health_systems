5b2a6df3f511debab39eeeb447ac095c46fc8915
carlosHF9 <chenrique452@gmail.com>
2021-10-05 11:06:34 -0300

    corrigido o bug do teclado afetando o tamanho da tela no android

--- front_end/holo-health-systems/src/pages/login/index.js
+++ front_end/holo-health-systems/src/pages/login/index.js
@@ -1,18 +1,28 @@
 import './style.css'
 import LoginForm from '../../sharable-components/login-form'
 import { ReactComponent as Logo } from '../../media/logo.svg'
+import { useState, useEffect } from 'react'
 
 
 function LoginPage() {
+
+    useEffect(() => {
+
+
+        
+    })
+
     return (
         <div className="login-page">
             <div className="hero-container">
                 <p >Frase de efeito que ainda vou decidir qual será</p>
                 <img className="img"src="https://www.healthpeople.org/wp-content/uploads/2015/01/HLtop.jpg" />
             </div>
+            
             <div className="login-container">
                 <Logo className="logo"/>
                 <LoginForm />
+                
             </div>
             
         </div>
