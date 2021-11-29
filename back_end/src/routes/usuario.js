003f53aa7be1bc1a3aeefa40e42d2593a6a8a5a4
wenderu00 <mwmcj@cin.ufpe.br>
2021-11-29 18:48:13 -0300

    rota de cadastro de gerente e correções no login

--- back_end/src/routes/usuario.js
+++ back_end/src/routes/usuario.js
@@ -70,7 +70,67 @@ router.post('/cadastro',(req,res,next)=>{
         )
     })
 })
-
+router.post("/cadastro/gerente",(req,res,next)=>{
+    pool.getConnection((error,conn)=>{
+        if(error){return res.status(500).send({error:error})}
+        conn.query(
+            'SELECT * FROM usuario WHERE cpf = ?',
+            [req.body.cpf],
+            (error,results,fields)=>{
+                if(error){return res.status(500).send({error:error})}
+                if(results.length>0){return res.status(409).send({mensagem: "cpf já cadastrado"})}
+                conn.query(
+                    'SELECT * FROM usuario WHERE email = ?',
+                    [req.body.email],
+                    (error,result,field)=>{
+                        if(error){return res.status(500).send({error:error})}
+                        if(result.length>0){return res.status(409).send({mensagem: "email já cadastrado"})}
+                        conn.query("SELECT * FROM gerente WHERE id_consultorio=?",[req.body.id_consultorio],(error,results,fields)=>{
+                            if(error){return res.status(500).send({error:error})}
+                            if(results.length>0){return res.status(409).send({mensagem: "Consultório já possui gerente"})}
+                            const saltRounds = 5
+                        bcrypt.genSalt(saltRounds, function(err, salt){
+                            bcrypt.hash(req.body.senha, salt, (errorBcrypt,hash)=>{     
+                            if(errorBcrypt){return res.status(500).send({error:errorBcrypt.message})}
+                            
+                            conn.query(
+                                'INSERT INTO usuario (cpf,nome,sobrenome,dt_nascimento,email,senha) VALUES (?,?,?,?,?,?)',
+                                [
+                                    req.body.cpf,
+                                    req.body.nome,
+                                    req.body.sobrenome,
+                                    req.body.dt_nascimento,
+                                    req.body.email,
+                                    hash
+                                ],
+                                (error,result,field)=>{
+                                    if(error){return res.status(500).send({error:error})}
+                                    conn.query('INSERT INTO gerente (cpf_gerente,id_consultorio) VALUES (?,?)',[req.body.cpf,req.body.id_consultorio],(error,results,fields)=>{
+                                        conn.release()
+                                        if(error){return res.status(500).send({error:error})}
+                                        const response = {
+                                            mensagem: "Gerente cadastrado com sucesso",
+                                            usuarioCriado: {
+                                                cpf: req.body.cpf,
+                                                email: req.body.email,
+                                                id_consultorio:req.body.id_consultorio
+                                            }
+                                        }
+                                        return res.status(201).send(response)
+                                    })
+                                }
+                            )
+                            })
+                        })
+                        })
+                        
+                        
+                    }
+                )
+            }
+        )
+    })
+})
 /** Rota para login
  * 
  *  Formato para requisição
@@ -91,15 +151,19 @@ router.post('/login',(req,res,next)=>{
                 if(results.length<1){return res.status(401).send({mensagem: "falha na autenticação"})}
                 bcrypt.compare(req.body.senha,results[0].senha,(err,result)=>{
                     if(err){return res.status(401).send({mensagem: "falha na autenticação"})}
-                    
-                    conn.query("SELECT * FROM gerente WHERE cpf_gerente= ?",
+                    if(result){
+                        conn.query("SELECT * FROM gerente WHERE cpf_gerente= ?",
                         [results[0].cpf],
                         (error,result,field)=>{
+                            
                             if(error){return res.status(500).send({error:error})}
+                            
                             if(result.length==0){
                                 conn.query("SELECT * FROM medico WHERE cpf_medico=?",[results[0].cpf],(error,result,fields)=>{
                                     if(error){return res.status(500).send({error:error})}
+                                   
                                     if(result.length==0){
+                                        
                                         if(results[0].cpf=="62318902364"){
                                             key=process.env.MASTER_JWT_KEY
                                             tipo="Mestre"
@@ -108,9 +172,10 @@ router.post('/login',(req,res,next)=>{
                                             key=process.env.PACIENTE_JWT_KEY
                                             tipo="Paciente"
                                         }
+                                        
                                         const token =  jwt.sign(
                                             {
-                                                id_usuario: results[0].id_usuario,
+                                                
                                                 cpf: results[0].cpf,
                                                 email: results[0].email
                                             },
@@ -118,13 +183,15 @@ router.post('/login',(req,res,next)=>{
                                             {
                                                 expiresIn:"1h"
                                             })
-                                            return res.status(200).send({
+                                        
+                                        return res.status(200).send({
                                                 mensagem: "autenticado com sucesso",
                                                 token: token,
                                                 tipo: tipo
                                             })
                                     }
                                     else{
+                                        
                                         key=process.env.MEDICO_JWT_KEY
                                         tipo="Medico" 
                                         const token =  jwt.sign(
@@ -165,15 +232,12 @@ router.post('/login',(req,res,next)=>{
                                     })
                             }
                         })
+                    }else{
+                        return res.status(401).send({mensagem: "falha na autenticação"})
+                    }
                     
-                    if(results[0].cpf=="62318902364"){
-                        key=process.env.MASTER_JWT_KEY
-                        tipo="Mestre"
-                    }
-                    else{
-                        key=process.env.PACIENTE_JWT_KEY
-                        tipo="Paciente"
-                    }
+                    
+                    
                     
                 })
             })
