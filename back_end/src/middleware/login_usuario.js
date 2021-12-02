5a39b39c80170e81efcc17a1cbd2fa479d81e738
wenderu00 <mwmcj@cin.ufpe.br>
2021-12-02 14:15:33 -0300

    login usuario

---
+++ back_end/src/middleware/login_usuario.js
@@ -0,0 +1,25 @@
+const jwt =  require("jsonwebtoken");
+module.exports = (req,res,next)=>{
+    try{
+        const token = req.headers.authorization.split(' ')[1]
+        const decode = jwt.verify(token,process.env.PACIENTE_JWT_KEY)
+        req.usuario = decode
+        next()
+    } catch(error){
+        try{
+            const token = req.headers.authorization.split(' ')[1]
+            const decode = jwt.verify(token,process.env.MEDICO_JWT_KEY)
+            req.usuario = decode
+            next()
+        } catch(error){
+            try{
+                const token = req.headers.authorization.split(' ')[1]
+                const decode = jwt.verify(token,process.env.MESTRE_JWT_KEY)
+                req.usuario = decode
+                next()
+            } catch(error){
+                return res.status(401).send({mensagem:"usuário não autenticado"})
+            }
+        }
+    }
+}
